a62b9c286d7a3e95698b6c63b18dc7777698e483
mrkwskiti <markdog_@hotmail.com>
2018-12-05 02:49:11 +0700

    Add clk_divide (set 1 s)

---
+++ clk_divide.v
@@ -0,0 +1,25 @@
+module clk_divide(
+    clk_div,
+    clk
+);
+
+input clk;
+output clk_div;
+
+integer state;
+initial state = 1;
+
+reg clk_div;
+always @ (posedge clk)
+begin
+    if (state == 1) begin
+        clk_div = ~clk_div;
+    end
+
+    if (state == 1000000) begin // FPS = 1 / FPS * 500000
+        state = 0;
+    end
+    state = state + 1;
+end
+
+endmodule // clk_div
