541e6d9c646ad2f3d7c2bae942af2064454783b6
mrkwskiti <markdog_@hotmail.com>
2018-12-10 03:27:11 +0700

    Optimize clk

--- clk_divide.v
+++ clk_divide.v
@@ -16,7 +16,7 @@ begin
         clk_div = ~clk_div;
     end
 
-    if (state == 3000) begin // FPS = 1 / FPS * 500000
+    if (state == 700) begin // FPS = 1 / FPS * 500000
         state = 0;
     end
     state = state + 1;
