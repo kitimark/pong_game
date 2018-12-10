dfbe419986333024cfad23820d49477ba44c150f
mrkwskiti <markdog_@hotmail.com>
2018-12-10 18:38:05 +0700

    Fix bit state

--- clk_divide.v
+++ clk_divide.v
@@ -6,20 +6,15 @@ module clk_divide(
 input clk;
 output clk_div;
 
-integer state;
-initial state = 1;
+reg[8:0] state;
 
 reg clk_div;
 always @ (posedge clk)
 begin
-    if (state == 1) begin
+    if (state == 0) begin
         clk_div = ~clk_div;
     end
-
-    if (state == 700) begin // FPS = 1 / FPS * 500000
-        state = 0;
-    end
-    state = state + 1;
+    state = state + 1'b1;
 end
 
 endmodule // clk_div
