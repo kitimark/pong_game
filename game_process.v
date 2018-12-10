1d47f437996e5d6354b423833d0af4febaa86fdd
mrkwskiti <markdog_@hotmail.com>
2018-12-10 18:38:46 +0700

    Add pos_ball

--- game_process.v
+++ game_process.v
@@ -4,17 +4,25 @@ module game_process(
     player_down,
     player_right,
     player_left,
+    pos_ball,
     count,
     clk
 );
 
 parameter SIZE = 2;
 parameter WIDTH = 8;
+parameter BIT_OF_WIDTH = 3;
 
 output[15:0] matrix_out;
 reg[15:0] matrix_out;
 
 input[2:0] player_top, player_down, player_right, player_left;
+
+input[BIT_OF_WIDTH*2-1:0] pos_ball;
+wire[BIT_OF_WIDTH-1:0] x_pos, y_pos;
+
+assign x_pos = pos_ball[5:3];
+assign y_pos = pos_ball[2:0];
 
 input clk;
 input[2:0] count;
@@ -46,7 +54,12 @@ begin
 		matrix_out[0] = 1;
 		matrix_out[7] = 1;
 	end
-
+	// ball
+    if(count == y_pos) begin
+        for(i = 1; i < WIDTH-1; i = i + 1) begin
+            matrix_out[i] = (x_pos == i) ? 1 : matrix_out[i];
+        end
+    end
 end
 
 endmodule // game_process
