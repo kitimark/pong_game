bda0bfc9817e03d6c0c05c06dfe46c841969a837
mrkwskiti <markdog_@hotmail.com>
2018-12-12 11:44:12 +0700

    Chosen one

--- game_process.v
+++ game_process.v
@@ -2,9 +2,8 @@ module game_process(
     matrix_out,
     player_top,
     player_down,
-    player_right,
-    player_left,
-    pos_ball,
+    x_pos,
+	y_pos,
 	a_longggggg,
     count,
     clk
@@ -20,16 +19,14 @@ reg[15:0] matrix_out;
 output[7:0] a_longggggg;
 reg[7:0] a_longggggg;
 
-input[2:0] player_top, player_down, player_right, player_left;
+input[2:0] player_top, player_down;
 
-input[BIT_OF_WIDTH*2-1:0] pos_ball;
-wire[BIT_OF_WIDTH-1:0] x_pos, y_pos;
-
-assign x_pos = pos_ball[5:3];
-assign y_pos = pos_ball[2:0];
+input[BIT_OF_WIDTH-1:0] x_pos, y_pos;
 
 input clk;
 input[2:0] count;
+
+reg[7:0] top_block, down_block;
 
 integer i;
 
@@ -37,55 +34,46 @@ always @ (posedge clk)
 begin
 	matrix_out = 0;
 	a_longggggg = 0;
-    // right player
-    matrix_out[7] = (WIDTH-player_right-3 < count && WIDTH-player_right+SIZE-2 > count) ? 1 : 0;
-    // left player
-    matrix_out[0] = (player_left-1 < count && player_left+SIZE > count) ? 1 : 0;
     // top player
-	if(count == 0)begin
-        for(i = 0; i < WIDTH; i = i + 1)begin
-        	matrix_out[WIDTH - i - 1] = (player_top-1 < i && player_top+SIZE > i) ? 1 : 0;
-			if(player_top == 6)begin
-				matrix_out[1:0] = 2'b11;
-			end
-    	end
-        // conner
-		matrix_out[0] = 1;
-		matrix_out[7] = 1;
-		// around of the ball
-		if(y_pos == 1)begin
-			for(i = 1; i < WIDTH-1; i = i + 1)begin
-				if(i == x_pos)begin
-					a_longggggg[0] = matrix_out[i-1];
-					a_longggggg[1] = matrix_out[i];
-					a_longggggg[2] = matrix_out[i+1];
-				end
-			end
-		end
-	end
-    // down player
-	if(count == 7)begin
-    	for(i = 0; i < WIDTH; i = i + 1)begin
-        	matrix_out[i] = (player_down-1 < i && player_down+SIZE > i) ? 1 : 0;
-			if(player_down == 6)begin
-				matrix_out[7:6] = 2'b11;
-			end
-    	end
-        // conner
-		matrix_out[0] = 1;
-		matrix_out[7] = 1;
-		// around of the ball
-		if(y_pos == 6)begin
-			for(i = 1; i < WIDTH-1; i = i + 1)begin
-				if(i == x_pos)begin
-					a_longggggg[5] = matrix_out[i-1];
-					a_longggggg[6] = matrix_out[i];
-					a_longggggg[7] = matrix_out[i+1];
-				end
-			end
+	for(i = 0; i < WIDTH; i = i + 1)begin
+		top_block[WIDTH - i - 1] = (player_top-1 < i && player_top+SIZE > i) ? 1 : 0;
+		if(player_top == 6)begin
+			top_block[1:0] = 2'b11;
 		end
 	end
 
+    // down player
+	for(i = 0; i < WIDTH; i = i + 1)begin
+		down_block[i] = (player_down-1 < i && player_down+SIZE > i) ? 1 : 0;
+		if(player_down == 6)begin
+			down_block[7:6] = 2'b11;
+		end
+	end
+
+	if(y_pos == 1)begin
+		for(i = 1; i < WIDTH-1; i = i + 1)begin
+			if(i == x_pos)begin
+				a_longggggg[0] = (x_pos == 0) ? 0 : top_block[i-1];
+				a_longggggg[1] = top_block[i];
+				a_longggggg[2] = (x_pos == 7) ? 0 : top_block[i+1];
+			end
+		end
+	end
+	if(y_pos == 6)begin
+		for(i = 1; i < WIDTH-1; i = i + 1)begin
+			if(i == x_pos)begin
+				a_longggggg[5] = (x_pos == 0) ? 0 : down_block[i-1];
+				a_longggggg[6] = down_block[i];
+				a_longggggg[7] = (x_pos == 7) ? 0 : down_block[i+1];
+			end
+		end
+	end
+	if(count == 0)begin
+		matrix_out = top_block;
+	end
+	if(count == 6)begin
+		matrix_out = down_block;
+	end
 end
 
 endmodule // game_process
