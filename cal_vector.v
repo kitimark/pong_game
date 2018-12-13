42d8c52268cb8820259a16eb4f362abc532fbc4d
mrkwskiti <markdog_@hotmail.com>
2018-12-14 02:46:37 +0700

    Fix cal vector

--- cal_vector.v
+++ cal_vector.v
@@ -5,8 +5,11 @@ module cal_vector(
     x_pos,
 	y_pos,
     state_top,
-    state_down
+    state_down,
+	clk
 );
+
+input clk;
 
 input[2:0] x_pos, y_pos;
 
@@ -15,7 +18,7 @@ input[2:0] state_top, state_down;
 output out_x, out_y, endgame;
 reg out_x, out_y, endgame;
 
-always @ (x_pos or y_pos)
+always @ (posedge clk)
 begin
     case (x_pos)
         3'b000 : out_x = 0;
